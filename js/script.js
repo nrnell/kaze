d2e228b3524d8d3045157a782e213b434d8f10ca
Nёll <kazuki_osada@outlook.jp>
2024-07-29 21:04:19 +0900

    Commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-let data = {}
-if (!localStorage.hasOwnProperty('kaze')) {
-    data['uuid'] = crypto.randomUUID()
-    data['name'] = prompt('ユーザー名')
+let data = {};
+if(!localStorage.hasOwnProperty('kaze')){
+    data['uuid'] = crypto.randomUUID();
+    data['name'] = null;
+    while(!data['name']){data['name'] = prompt('ユーザー名');};
     localStorage.setItem('kaze',JSON.stringify(data));
 }
 data = JSON.parse(localStorage.getItem('kaze'));
