0f82a93416d18e0cf95e686509bdddb3675b1573
Nёll <kazuki_osada@outlook.jp>
2024-07-29 21:52:31 +0900

    Commit

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ if(!localStorage.hasOwnProperty('kaze')){
     data['name'] = null;
     while(!data['name']){data['name'] = prompt('ユーザー名');};
     localStorage.setItem('kaze',JSON.stringify(data));
-}
+};
 data = JSON.parse(localStorage.getItem('kaze'));
+const socket = new WebSocket('https://nrnell.github.io/kaze/');
+socket.onopen = function(e){};
+socket.onerror = function(e){};
+socket.onmessage = function(e){console.log(e.data);};
+socket.onclose = function(){};
+let form = document.querySelector('.chat');
+form.onsubmit = function(e){
+    e.preventDefault();
+    socket.send(form.message.value);
+};
