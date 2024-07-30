06e3e110802716b22c950bc1a60215406cb71de1
Nёll <kazuki_osada@outlook.jp>
2024-07-30 13:18:02 +0900

    Commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,3 @@ if(!localStorage.hasOwnProperty('kaze')){
     localStorage.setItem('kaze',JSON.stringify(data));
 };
 data = JSON.parse(localStorage.getItem('kaze'));
-const socket = new WebSocket(location.host);
-socket.onopen = function(e){
-    console.log('接続成功');
-};
-socket.onerror = function(e){};
-socket.onmessage = function(e){console.log(e.data);};
-socket.onclose = function(){};
-let form = document.querySelector('.chat');
-form.onsubmit = function(e){
-    e.preventDefault();
-    socket.send(form.message.value);
-};
